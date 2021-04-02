9b8a6352ec031d10cbe53184780d9aa0548b7481
StamplerJ <jann.luellmann@web.de>
2021-04-02 16:00:11 +0100

    Refactor

---
+++ Assets/Scripts/Grid/SnapToGrid.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+
+public class SnapToGrid : MonoBehaviour
+{
+    [SerializeField] private bool isMovable = true;
+    [SerializeField] private float velocity = 50f;
+    [SerializeField] private Vector3 offset;
+
+    private GridGenerator grid;
+
+    private Vector3 snapPosition;
+    private float traveledTime;
+
+    private bool isSelected = false;
+
+    private void Awake()
+    {
+        grid = GameObject.Find(Names.Grid).GetComponent<GridGenerator>();
+
+        snapPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if (!isMovable)
+            return;
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject.Equals(this.gameObject))
+                {
+                    isSelected = true;
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isSelected = false;
+            transform.position = snapPosition;
+        }
+
+        if (isSelected)
+        {
+            UpdatePosition();
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (isSelected && Input.GetMouseButton(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.tag.Equals(Tags.Stackable))
+                {
+                    SnapToPosition(hit.point, hit.collider.gameObject);   
+                }
+            }
+        }
+    }
+
+    private void SnapToPosition(Vector3 position, GameObject hitObject)
+    {
+        Vector3 targetPos = grid.GetNearestPointOnGrid(position);
+        // targetPos += new Vector3(0f, transform.localScale.y / 2f, 0f);
+        snapPosition = targetPos + offset;
+
+        if (!hitObject.name.Equals(Names.Floor) && hitObject.gameObject != this.gameObject)
+        {
+            snapPosition += new Vector3(0f, hitObject.transform.localScale.y, 0f);
+        }
+        
+        traveledTime = 0f;
+    }
+
+    private void UpdatePosition()
+    {
+        traveledTime += Time.deltaTime * velocity;
+        transform.position = Vector3.Lerp(transform.position, snapPosition, traveledTime);
+    }
+
+    public Vector3 GetCenterPosition()
+    {
+        return transform.position - offset;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(GetCenterPosition(), 0.5f);
+    }
+}
---
+++ Assets/Scripts/MechanicsObjects/RocketEngine.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketEngine : MonoBehaviour
+{
+    private new Rigidbody rigidbody;
+
+    [SerializeField] private float speed = 50f;
+    [SerializeField] private GameObject engine;
+    [SerializeField] private GameObject front;
+
+    private float deltaPos;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddForce(GetDirection() * speed);
+        }
+
+        // Freeze rotation manually
+        transform.eulerAngles = new Vector3(0f, transform.localEulerAngles.y, transform.localEulerAngles.z);
+    }
+
+    public Vector3 GetDirection()
+    {
+        return (front.transform.position - engine.transform.position).normalized;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(front.transform.position, front.transform.position + GetDirection());
+    }
+}
